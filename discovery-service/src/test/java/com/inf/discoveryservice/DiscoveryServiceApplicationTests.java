34f0edd8ae5fa01039b571e917836c806508035b
ygyoo <ygyoo@mtdata.co.kr>
2024-06-17 15:13:23 +0900

    MSA강의 공부 최초

---
+++ discovery-service/src/test/java/com/inf/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inf.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
