03adc89e24824848f2b810f90ce562783705f776
ygyoo <ygyoo@mtdata.co.kr>
2024-06-17 16:14:31 +0900

    gateway filter 추가

---
+++ user-service/src/main/java/com/inf/userservice/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.inf.userservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user-service")
+@Slf4j
+public class UserController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(@RequestHeader("user-request") String userRequest) {
+        return ResponseEntity.ok(userRequest);
+    }
+}
