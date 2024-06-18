1ac1bbcf99c0b05c720603c6b7e415bb4d52a5a1
ygyoo <ygyoo@mtdata.co.kr>
2024-06-18 10:04:30 +0900

    LoggingFilter 추가

--- user-service/src/main/java/com/inf/userservice/controller/UserController.java
+++ user-service/src/main/java/com/inf/userservice/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.inf.userservice.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -10,7 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user-service")
 @Slf4j
+@RequiredArgsConstructor
 public class UserController {
+
+    private final Environment environment;
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello(@RequestHeader("user-request") String userRequest) {
@@ -18,8 +24,10 @@ public class UserController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "user-service/check!";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("user-service/check! port=%s", environment.getProperty("server.port"));
     }
 
 }
